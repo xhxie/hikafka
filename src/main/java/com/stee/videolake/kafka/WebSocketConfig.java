4fc4e56a411908cda494107f980e7327120ee5d7
xhxie <xhxie@yahoo.com>
2021-05-17 11:09:28 +0800

    Change WebSocketConfig package

--- src/main/java/com/stee/videolake/kafka/WebSocketConfig.java
+++ src/main/java/com/stee/videolake/kafka/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.stee.videolake.websocket;
+package com.stee.videolake.kafka;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
